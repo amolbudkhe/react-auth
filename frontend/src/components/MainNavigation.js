35e1a290da1f8426d9083dd9dd8cae268230227c
amolbudkhe <abudkhe>
2023-06-28 11:27:21 +0530

    IM queryparameter for switching login & signup

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -40,6 +40,16 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
+          <li>
+            <NavLink
+              to="/auth?mode=login"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Authentications
+            </NavLink>
+          </li>
         </ul>
       </nav>
       <NewsletterSignup />
