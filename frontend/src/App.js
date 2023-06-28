877cfd099110597c5645f84d74097c24d8b35d54
amolbudkhe <abudkhe>
2023-06-28 11:47:30 +0530

    IM create user funtionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,12 @@ import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
 import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
-import { action as manipulateEventAction } from "./components/EventForm";
+import {
+  action,
+  action as manipulateEventAction,
+} from "./components/EventForm";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, { AuthAction } from "./pages/Authentication";
 
 const router = createBrowserRouter([
   {
@@ -61,7 +64,7 @@ const router = createBrowserRouter([
         action: newsletterAction,
       },
       ,
-      { path: "auth", element: <AuthenticationPage /> },
+      { path: "auth", element: <AuthenticationPage />, action: AuthAction },
     ],
   },
 ]);
